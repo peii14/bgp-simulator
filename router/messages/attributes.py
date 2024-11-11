1adefbfcf7db3911c2d29b0fb0c38ea177785865
Alexandre Oliveira <1251067+Xinayder@users.noreply.github.com>
2024-11-11 21:09:07 +0200

    WIP: chore: add BGP UPDATE message (#12)
    * chore: add BGP UPDATE message
    
    * chore: allow BGP path attributes to be created
    
    * fix: remove hardcoded values in OPEN message
    
    ---------
    
    Co-authored-by: Alexandre Almeida <Xinayder@users.noreply.github.com>

---
+++ router/messages/attributes.py
@@ -0,0 +1,113 @@
+import ipaddress
+import struct
+
+from enum import Enum
+
+ATTR_OPTIONAL = 1 << 7
+ATTR_TRANSITIVE = 1 << 6
+ATTR_PARTIAL = 1 << 5
+ATTR_EXTENDED = 1 << 4
+
+class BgpAttributeType(Enum):
+        ORIGIN = 1
+        AS_PATH = 2
+        NEXT_HOP = 3
+        MULTI_EXIT_DISC = 4
+        LOCAL_PREF = 5
+        ATOMIC_AGGREGATE = 6
+        AGGREGATOR = 7
+
+class BgpPathAttribute():
+    def __init__(self, attr_type=-1, data={}):
+        if attr_type == -1 or len(data) == 0:
+            return
+
+        match BgpAttributeType(attr_type):
+            case BgpAttributeType.ORIGIN:
+                self.flags = ATTR_TRANSITIVE
+                self.length = 1
+            case BgpAttributeType.AS_PATH:
+                self.flags = ATTR_TRANSITIVE
+                self.length = 2 + 2 * len(data['asns'])
+            case BgpAttributeType.NEXT_HOP:
+                self.flags = ATTR_TRANSITIVE
+                self.length = 4
+            case BgpAttributeType.MULTI_EXIT_DISC:
+                self.flags = ATTR_OPTIONAL
+                self.length = 4
+            case _:
+                pass
+
+        self.type = BgpAttributeType(attr_type)
+        self.data = data
+
+    def frombytes(self, path_attr_bytes):
+        flags = int(path_attr_bytes[0])
+        attr_type = int(path_attr_bytes[1])
+
+        self.optional = bool(flags & ATTR_OPTIONAL)
+        self.transitive = bool(flags & ATTR_TRANSITIVE)
+        self.partial = bool(flags & ATTR_PARTIAL)
+        self.extended = bool(flags & ATTR_EXTENDED)
+        self.flags = flags
+
+        length_idx = 3 if not self.extended else 4
+
+        self.type = BgpAttributeType(attr_type)
+        self.length = int(path_attr_bytes[2]) if not self.extended else int(path_attr_bytes[2:4])
+
+        attr_bytes = path_attr_bytes[length_idx:]
+
+        match BgpAttributeType(self.type):
+            case BgpAttributeType.ORIGIN:
+                self.data = {
+                    "origin": int(attr_bytes[0])
+                }
+            case BgpAttributeType.AS_PATH:
+                len_asn, seg_type = struct.unpack("!2B", attr_bytes[:2])
+                asn_tuple = struct.unpack(f"!{len_asn}H", attr_bytes[2:])
+
+                asns = []
+                asns.extend(asn_tuple)
+
+                self.data = {
+                    "length": len_asn,
+                    "segment_type": seg_type,
+                    "asns": asns,
+                }
+            case BgpAttributeType.NEXT_HOP:
+                self.data = {
+                    "ip_addr": ipaddress.IPv4Address(struct.unpack("!I", attr_bytes[0:4])[0])
+                }
+            case BgpAttributeType.MULTI_EXIT_DISC:
+                self.data = {
+                    "metric": struct.unpack("!I", attr_bytes[0:4])[0]
+                }
+            case _:
+                self.data = {}
+    
+    def pack(self) -> bytes:
+        header = struct.pack("!3B", self.flags, self.type.value, self.length)
+        payload = header
+
+        match BgpAttributeType(self.type):
+            case BgpAttributeType.ORIGIN:
+                payload += struct.pack("!B", self.data['origin'])
+            case BgpAttributeType.AS_PATH:
+                packed_asns = bytes(0)
+                for _,asn in enumerate(self.data['asns']):
+                    packed_asns += struct.pack("!H", asn)
+
+                payload += struct.pack(f"!2B", self.data['length'], self.data['segment_type'])
+                payload += packed_asns
+            case BgpAttributeType.NEXT_HOP:
+                payload += struct.pack("!4s", self.data['ip_addr'].packed)
+            case BgpAttributeType.MULTI_EXIT_DISC:
+                payload += struct.pack("!I", self.data['metric'])
+            case _:
+                pass
+
+        return payload
+
+    def __str__(self):
+        return f"<BgpAttribute flags={self.flags} type={self.type} length={self.length} data={self.data} />"
--- router/messages/message_base.py
+++ router/messages/message_base.py
@@ -2,6 +2,8 @@ import struct
 import ipaddress
 from enum import Enum
 
+from router.messages.attributes import *
+
 class BgpMessageType(Enum):
     OPEN = 1
     UPDATE = 2
@@ -64,7 +66,6 @@ class BgpMessageBase():
 class BgpMessageOpen(BgpMessageBase):
     def __init__(self, ip="192.168.0.1", version=4, hold_time=0, as_num=0):
         super().__init__(msg_type=BgpMessageType.OPEN)
-        self.payload_fmt = "!BHHIB"
         self.ip_addr = ipaddress.IPv4Address(ip)
         self.version = version
         self.hold_time = hold_time
@@ -72,10 +73,10 @@ class BgpMessageOpen(BgpMessageBase):
 
     def payload(self):
         return struct.pack(
-            self.payload_fmt,
-            4,
-            65033,
-            180,
+            "!BHHIB",
+            self.version,
+            self.as_number,
+            self.hold_time,
             int(self.ip_addr),
             0
         )
@@ -131,5 +132,109 @@ class BgpMessageNotification(BgpMessageBase):
 
 
 class BgpMessageUpdate(BgpMessageBase):
-    def __init__(self):
-        pass
+    def __init__(self, withdrawn_routes=[], path_attributes=[], nlri=[]):
+        super().__init__(msg_type=BgpMessageType.UPDATE)
+
+        self.withdrawn_routes = withdrawn_routes
+        self.path_attributes = path_attributes
+        self.nlri = nlri
+
+    def payload(self):
+        def pack_attrs():
+            results = bytes(0)
+            count = 0
+            packed_attr = bytes(0)
+            packed_nlri = bytes(0)
+            for _, attr in enumerate(self.path_attributes):
+                count += 3
+                count += attr.length
+                packed_attr += attr.pack()
+
+            for _, nlri in enumerate(self.nlri):
+                nlri_addr_len = int(4 - (32 -  nlri.prefixlen) / 8)
+
+                nlri_bytes = bytes(nlri.network_address.packed[0:nlri_addr_len])
+                packed_nlri += struct.pack(f"!B{nlri_addr_len}s", nlri.prefixlen, nlri_bytes)
+
+            results += struct.pack("!H", count)
+            results += packed_attr
+            results += packed_nlri
+            return results
+
+        return struct.pack(
+            f"!H{len(self.withdrawn_routes)}s",
+            len(self.withdrawn_routes),
+            self.withdrawn_routes if len(self.withdrawn_routes) else bytes(0),
+        ) + pack_attrs()
+
+    def unpack(self, byte_str):
+        super().unpack(byte_str)
+        byte_str = byte_str[19:]
+        len_withdrawn_routes = struct.unpack("!H", byte_str[0:2])[0]
+
+        if len_withdrawn_routes > 0:
+            raise NotImplementedError("path withdrawal is not implemented")
+
+        byte_str = byte_str[2:]
+        len_path_attr = struct.unpack("!H", byte_str[0:2])[0]
+
+        if len_path_attr > 0:
+            byte_str = byte_str[2:]
+            i = 0
+            while True:
+                if i >= len_path_attr:
+                    break
+
+                path_bytes = byte_str[i:]
+
+                flags = byte_str[i]
+                attr_type = int(byte_str[i + 1])
+                attr_len = int(byte_str[i + 2])
+
+                print("attr flags:", flags, attr_type, attr_len)
+
+                match BgpAttributeType(attr_type):
+                    case BgpAttributeType.ORIGIN:
+                        path_bytes = path_bytes[:4]
+                        i += 4
+                    case BgpAttributeType.AS_PATH:
+                        num_asns = int(byte_str[i + 4])
+                        path_bytes = path_bytes[:5 + (num_asns * 2)]
+                        i += 5 + (num_asns * 2)
+                    case BgpAttributeType.NEXT_HOP | BgpAttributeType.MULTI_EXIT_DISC:
+                        path_bytes = path_bytes[:7]
+                        i += 7
+                    case _:
+                        break
+
+                path_attr = BgpPathAttribute()
+                path_attr.frombytes(path_bytes)
+                self.path_attributes.append(path_attr)
+
+        # NLRI length is 23 - total path attrs length - withdrawn routes length
+        len_nlri = self.raw_length - 23 - len_path_attr - len_withdrawn_routes
+        byte_str = byte_str[len_path_attr:]
+
+        hex_str = byte_str.hex().upper()
+        print(' '.join(hex_str[i:i+2] for i in range(0, len(hex_str), 2)))
+
+        i = 0
+        while True:
+            if i >= len_nlri:
+                break
+            # NLRI is a tuple with prefix, addr
+            nlri_prefix = struct.unpack(f"!B", byte_str[i:1])[0]
+            nlri_addr_len = int(4 - (32 -  nlri_prefix) / 8)
+
+            nlri_addr = struct.unpack(f"!{nlri_addr_len}s", byte_str[i + 1:nlri_addr_len + 1])[0]
+            host_bits = 4 - nlri_addr_len
+            # add trailing zeroes to correspond to hostmask
+            nlri_addr += host_bits * b'\x00'
+
+            self.nlri.append(ipaddress.IPv4Network((nlri_addr, nlri_prefix)))
+
+            i += nlri_addr_len + 1
+            byte_str = byte_str[i:]
+
+    def __str__(self):
+        return f"<BGPMessage type={self.msg_type} length={self.length()} withdrawn_routes={self.withdrawn_routes} path_attributes={self.path_attributes} nlri={self.nlri}>"
